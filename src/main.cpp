a5b4f2fb585522ce92388a4d6c3dbd8e3244e995
Sávio Carlos Martins Costa <saviocmc@protonmail.com>
2017-04-23 00:00:02 -0300

    First implementation

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,19 @@
 #include <Arduino.h>
+#include <Relay.h>
+
+Relay lamp(9);
+
+uint8_t const doorButtonPin = 10;
+
+bool isDoorButtonPressed() {
+	return !digitalRead(doorButtonPin);
+}
 
 void setup() {
-
+	pinMode(doorButtonPin, INPUT_PULLUP);
 }
 
 void loop() {
-
+	if (isDoorButtonPressed()) lamp.setState(ON);
+	else lamp.setState(OFF);
 }
