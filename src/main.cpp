ec7eabf8a5dcafd000f37e5038ee543533c9d6a9
Sávio Carlos Martins Costa <saviocmc@protonmail.com>
2017-04-23 12:44:43 -0300

    Changing the initial "timeout" from 10 to 5 minutes Putting the statement that turn ON the lamp as soon as possible.

--- src/main.cpp
+++ src/main.cpp
@@ -14,17 +14,17 @@ uint8_t const doorButtonPin = 10;
 // This is the time (in milliseconds) that the lamp will stay ON if (and only if) when the microcontroller
 // is turned on, the door is open. If the door is closed before these timeout is over
 // then it won't matter anymore and the lamp will be turned off as soon as the door is open again.
-uint64_t const timeout = 600000; // 10 minutes
+uint64_t const timeout = 300000; // 5 minutes
 
 bool isTheDoorOpen() {
 	return digitalRead(doorButtonPin);
 }
 
 void setup() {
-	pinMode(doorButtonPin, INPUT_PULLUP);
 	// When the microcontroller is turned on, the lamp should be turned on as well
 	// no matters if the door is open or close.
 	lamp.setState(ON);
+	pinMode(doorButtonPin, INPUT_PULLUP);
 	// Let the lamp stay ON until the door is closed for the first time or the initial timeout is over.
 	while (isTheDoorOpen() && millis() < timeout); // Wait here while the door still open and the timeout is not over.
 	//Once the door is closed (or the timeout over), the "loop" will take place.
