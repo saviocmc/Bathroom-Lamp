275c52586a60097c57a8cf926bb6b8032be86d18
Sávio Carlos Martins Costa <saviocmc@protonmail.com>
2017-04-23 01:18:00 -0300

    Changing the definition of the "timeout". Adding more comments.

--- src/main.cpp
+++ src/main.cpp
@@ -7,12 +7,13 @@
 
 Relay lamp(9);
 
+// The pin in which the door's "button" is attached to.
 uint8_t const doorButtonPin = 10;
 
-// This is the time that the lamp will stay ON if (and only if) when the microcontroller
+// This is the time (in milliseconds) that the lamp will stay ON if (and only if) when the microcontroller
 // is turned on, the door is open. If the door is closed before these timeout is over
 // then it won't matter anymore and the lamp will be turned off as soon as the door is open again.
-uint64_t const timeout = 10 * 60 * 1000; // 10 minutes
+uint64_t const timeout = 600000; // 10 minutes
 
 bool isTheDoorOpen() {
 	return digitalRead(doorButtonPin);
@@ -23,9 +24,9 @@ void setup() {
 	// When the microcontroller is turned on, the lamp should be turned on as well
 	// no matters if the door is open or close.
 	lamp.setState(ON);
-	// Let the lamp stay ON until the door is closed for the first time or the initial timeout is over
+	// Let the lamp stay ON until the door is closed for the first time or the initial timeout is over.
 	while (isTheDoorOpen() && millis() < timeout); // Wait here while the door still open and the timeout is not over.
-	//Once the door is closed (or the timeout over), the "loop" will take place
+	//Once the door is closed (or the timeout over), the "loop" will take place.
 }
 
 void loop() {
