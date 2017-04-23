19dcae6d60674c6aaaba40eaadb70da913b12675
Sávio Carlos Martins Costa <saviocmc@protonmail.com>
2017-04-23 03:34:41 -0300

    Creating the "performance" branch 	This branch will not use any library but the "digitalWriteFast" macros. 	This is a test to see if the use of the Relay library causes a sensitive startup delay.

--- src/main.cpp
+++ src/main.cpp
@@ -1,39 +1,25 @@
 /**
 * @author Sávio Carlos Martins Costa
+*
+* This is the "performance" branch. All code here will be minimized
+* and optimized to get the minimum execution time.
 **/
 
 #include <Arduino.h>
-#include <Relay.h>
+#include <digitalWriteFast.h>
 
-// Instantiate a new Relay object attached to pin 9.
-Relay lamp(9);
-
-// The pin in which the door's "button" is attached to.
+uint8_t const relayPin = 9;
 uint8_t const doorButtonPin = 10;
-
-// This is the time (in milliseconds) that the lamp will stay ON if (and only if) when the microcontroller
-// is turned on, the door is open. If the door is closed before these timeout is over
-// then it won't matter anymore and the lamp will be turned off as soon as the door is open again.
 uint64_t const timeout = 600000; // 10 minutes
 
-bool isTheDoorOpen() {
-	return digitalRead(doorButtonPin);
-}
-
 void setup() {
-	pinMode(doorButtonPin, INPUT_PULLUP);
-	// When the microcontroller is turned on, the lamp should be turned on as well
-	// no matters if the door is open or close.
-	lamp.setState(ON);
-	// Let the lamp stay ON until the door is closed for the first time or the initial timeout is over.
-	while (isTheDoorOpen() && millis() < timeout); // Wait here while the door still open and the timeout is not over.
-	//Once the door is closed (or the timeout over), the "loop" will take place.
+	pinModeFast(relayPin, OUTPUT);
+	pinModeFast(doorButtonPin, INPUT_PULLUP);
+	digitalWriteFast(relayPin, LOW);
+	while (digitalReadFast(doorButtonPin) && millis() < timeout);
 }
 
 void loop() {
-	if (isTheDoorOpen()) lamp.setState(OFF);
-	else lamp.setState(ON);
-	// This delay here is just to prevent the relay to be toggled too many times
-	// in the period when the button (the door) is opening or closing.
+	digitalWriteFast(relayPin, digitalReadFast(doorButtonPin));
 	delay(100);
 }
