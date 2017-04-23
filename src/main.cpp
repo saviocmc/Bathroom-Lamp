ca08bb0c2e31ed705a46ce30e9370c9b4c9c8689
Sávio Carlos Martins Costa <saviocmc@protonmail.com>
2017-04-23 01:09:50 -0300

    Adding comments explaining the algorithm.

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,7 @@
+/**
+* @author Sávio Carlos Martins Costa
+**/
+
 #include <Arduino.h>
 #include <Relay.h>
 
@@ -5,16 +9,27 @@ Relay lamp(9);
 
 uint8_t const doorButtonPin = 10;
 
-bool isDoorButtonPressed() {
-	return !digitalRead(doorButtonPin);
+// This is the time that the lamp will stay ON if (and only if) when the microcontroller
+// is turned on, the door is open. If the door is closed before these timeout is over
+// then it won't matter anymore and the lamp will be turned off as soon as the door is open again.
+uint64_t const timeout = 10 * 60 * 1000; // 10 minutes
+
+bool isTheDoorOpen() {
+	return digitalRead(doorButtonPin);
 }
 
 void setup() {
 	pinMode(doorButtonPin, INPUT_PULLUP);
+	// When the microcontroller is turned on, the lamp should be turned on as well
+	// no matters if the door is open or close.
+	lamp.setState(ON);
+	// Let the lamp stay ON until the door is closed for the first time or the initial timeout is over
+	while (isTheDoorOpen() && millis() < timeout); // Wait here while the door still open and the timeout is not over.
+	//Once the door is closed (or the timeout over), the "loop" will take place
 }
 
 void loop() {
-	if (isDoorButtonPressed()) lamp.setState(ON);
-	else lamp.setState(OFF);
+	if (isTheDoorOpen()) lamp.setState(OFF);
+	else lamp.setState(ON);
 	delay(100);
 }
